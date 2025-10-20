14b54bcebecf93dbd9786742cba86ce0e588961f
mBarneto <4347791+m-barneto@users.noreply.github.com>
2025-10-20 10:27:15 -0400

    Updated to 4.0.x

--- ReachExtender/ReachExtender.cs
+++ ReachExtender/ReachExtender.cs
@@ -4,7 +4,7 @@ using ReachExtender.Patches;
 using UnityEngine;
 
 namespace ReachExtender {
-    [BepInPlugin("Mattdokn.ReachExtender", "ReachExtender", "1.4.0")]
+    [BepInPlugin("Mattdokn.ReachExtender", "ReachExtender", "1.4.1")]
     public class ReachExtenderPlugin : BaseUnityPlugin {
 
         public static ConfigEntry<float> InteractionDistance;
